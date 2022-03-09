6c17ca663a5121bb2bdff5fba2f4590556745301
nahyln <elinn785@gmail.com>
2022-03-09 12:47:18 -0400

    Create busqueda_binaria.py

---
+++ busqueda_binaria.py
@@ -0,0 +1,25 @@
+
+def bin_(n,z):
+    l = 0 #límite inferior
+    h = len(n)-1 #longitud del arreglo como límite superior inicial
+    m = 0 #punto medio
+    #ordenar arreglo:
+    n.sort()
+    while(True):
+        m = round(l+((h-l)/2)) #fórmula para calcular el punto medio
+        if(n[m] == z):
+            print(n[m])
+            break
+        if(n[m] < z): #si punto medio es menor al número buscado, se suma 1 para mover la zona de búsqueda hacia la derecha
+            l = m + 1
+        if(n[m] > z): #si punto medio es menor al número buscado, se resta 1 para mover la zona de búsqueda hacia la izquierda
+            h = m - 1     
+        if(l > h): 
+            print("El numero no existe en el arreglo")
+            break
+        
+
+#donde A: un arreglo de números enteros.
+# B = número entero a buscar en el arreglo.
+
+#bin_(A,B)
